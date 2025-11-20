017abfc3c8b33009b087ce282909136aafcafabb
ANKIT KUSHWAHA <ankitrajank247@gmail.com>
2025-11-20 14:02:56 +0530

    19 11 evening

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# 1. Install Requirements
+pip install -r requirements.txt
+
+# 2. Collect Static Files
+python manage.py collectstatic --no-input
+
+# 3. Migrate Database (Create tables)
+python manage.py migrate
+
+# 4. Load Your Data (The new part)
+# ⚠️ WARNING: This overwrites data with matching IDs!
+python manage.py loaddata data.json
