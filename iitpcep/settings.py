ae975c916abdfd68a0af2df0e368eb8a2e3ef1c1
ANKIT KUSHWAHA <ankitrajank247@gmail.com>
2025-11-28 00:16:57 +0530

    Add allowed host

--- iitpcep/settings.py
+++ iitpcep/settings.py
@@ -25,6 +25,7 @@ ALLOWED_HOSTS = [
     "www.iitpcep.online",
     "iitpcep-online.onrender.com",
     "cet.iitpcep.online",
+    "test.iitpcep.online",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
@@ -32,6 +33,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://www.iitpcep.online",
     "https://iitpcep-online.onrender.com",
     "https://cet.iitpcep.online",
+    "https://test.iitpcep.online/"
 ]
 
 LOGIN_URL = 'admin_dashboard:admin_login'
